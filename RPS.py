034bb8b9c60fc7b494e62b9afd09f1eb9aae4a23
unknown <edisonalexanderarias@gmail.com>
2023-03-04 00:02:37 -0800

    Added a dictionary

---
+++ RPS.py
@@ -0,0 +1,11 @@
+def get_choices():
+    player_choice = "rock"
+    computer_choice = "paper"
+    choices{"player": player_choice}
+
+    return player_choice
+
+choices = get_choices
+print(choices)
+
+dict = {"name":"","","": choices}
